0912ea8c2882f4400026bbdb3e5d10b613b60a74
HAMID KAMAL <rahmani22052009@gmail.com>
2026-05-20 19:42:07 +0530

    Add initial Python scripts for variables, data types, operators, and variable rules

---
+++ datatypes.py
@@ -0,0 +1,16 @@
+a = 2 # integer
+
+b = 3.98 # float
+
+c = " Hamid" # string
+
+d = True # boolean
+
+e = None  # NoneType
+
+
+print(type(a))
+print(type(b))
+print(type(c))      
+print(type(d))
+print(type(e))
---
+++ rule_variable.py
@@ -0,0 +1,12 @@
+name = 3
+
+a  = 543
+
+_kamal = 4
+
+kamal_43 =45
+
+# @hamid = 843   
+
+# hamid@=  453
+
